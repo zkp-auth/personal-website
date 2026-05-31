7f9767f5d287b07c7b433bba38e711a2a229c036
NI09 <echos16_pepper@icloud.com>
2026-05-31 02:49:49 +0200

    feat: add profile picture

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
+import profilePicture from './assets/images/profile-nacir.jpg'
 
 
 function App() {
   
   return (
     <main>
-      <>Nacir Ibraimo</>
-      <p>Fullstack Developer</p>
+      <div className="card">
+        <img src={profilePicture} alt="Nacir" />
+        <div>
+          <h3>Nacir Ibraimo</h3>
+          <p>Fullstack Developer</p>
+        </div>
+        
+      </div>
+      
     </main>
   )
 }

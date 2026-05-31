665556605f35da4a9219be2871610a645d1c45d9
NI09 <echos16_pepper@icloud.com>
2026-05-31 19:43:47 +0200

    feat: add profile card

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,30 @@ function App() {
   return (
     <main>
       <div className="card">
-        <img src={profilePicture} alt="Nacir" />
+        <img className="avatar" src={profilePicture} alt="Nacir" />
         <div>
           <h3>Nacir Ibraimo</h3>
           <p>Fullstack Developer</p>
+          <h4>
+            <a href="https://github.com/zkp-auth" target="_blank" rel="noreferrer">
+              @zkp-auth
+            </a>
+          </h4>
+          <h4>France, FR</h4>
         </div>
-        
       </div>
+      <div className="projects">
+        <div className="project" id="project-1">
+          <h4>Project One</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus
+            nihil aut aliquam quidem officiis voluptatibus consequuntur
+            exercitationem! Esse ut enim adipisci molestias, facilis quaerat
+            tenetur ad aperiam deleniti ea ex.
+          </p>
+        </div>
+      </div>
+
       
     </main>
   )
